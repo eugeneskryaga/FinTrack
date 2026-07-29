151a9546c4ec9e86d5ca7409acbda196403a40bf
Eugene <zhenyaskryaga@gmail.com>
2026-07-29 21:59:56 +0300

    add logo to header

--- src/app/layouts/Layout.tsx
+++ src/app/layouts/Layout.tsx
@@ -1,13 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../../widgets/Navbar/Navbar";
 
+import css from "./Layout.module.css";
+import { Logo } from "../../shared/components/Logo/Logo";
+
 export const Layout = () => {
   return (
     <>
-      <main>
+      <header className={css.header}>
+        <Logo />
+      </header>
+      <main className={css.main}>
         <Outlet />
       </main>
-      <footer>
+      <footer className={css.footer}>
         <Navbar />
       </footer>
     </>
